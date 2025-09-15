8c182bd02e91a9e13e493d5918766e6e54e3b5ef
MuslumKhalilov4 <muslumkhalilov04@gmail.com>
2025-09-15 16:30:04 +0400

    Project created. Category model and table created.

---
+++ app/Models/Category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Translatable\HasTranslations;
+
+class Category extends Model
+{
+    use HasFactory, HasTranslations, SoftDeletes;
+
+    protected $fillable = ['name', 'order'];
+    
+    protected $translatable = ['name']; 
+}
